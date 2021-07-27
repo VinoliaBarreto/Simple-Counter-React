044a05222e4e65a96647ad82904eb96260b7a8b9
VinoliaBarreto <vinolia.barreto@gmail.com>
2021-07-27 16:50:12 +0000

    Simple Counter React

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,14 +1,3 @@
-//import react into the bundle
-import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
-import { Home } from "./component/home.js";
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
@@ -35,6 +24,3 @@ Counter.propTypes = {
 };
 
 export default Counter;
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,34 @@
 import React from "react";
 
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Counter from "./counter.jsx";
+
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center d-flex justify-content-center mt-5">
+			<div className="row counterStyle">
+				<div className="col">
+					<i className="far fa-clock"></i>
+				</div>
+				<div className="col">
+					<Counter time={10000} />
+				</div>
+				<div className="col">
+					<Counter time={1000} />
+				</div>
+				<div className="col">
+					<Counter time={100} />
+				</div>
+				<div className="col">
+					<Counter time={10} />
+				</div>
+				<div className="col">
+					<Counter time={1} />
+				</div>
+			</div>
 		</div>
 	);
 }
